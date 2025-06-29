93f9d3f694f37acce00277f1965ba6ad12a7468f
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-06-29 20:35:42 +0545

    Refactor middleware to use createRouteMatcher for public routes and improve readability

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,44 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
 //----------My Public Routes----------//
-const isPublicRoutes = [
-    "/signin",
-    "/sign-up",
-    "/",
-    "/home"
-]
+const isPublicRoutes =createRouteMatcher([
+  "/signin",
+  "/sign-up",
+  "/",
+  "/home"
+])
 
 //---------Public API Routes----------//
-const isPublicApiRoutes = [
-    "/api/videos"
-]
+const isPublicApiRoutes = createRouteMatcher([
+  "/api/videos"
+])
 
 export default clerkMiddleware((auth, req) => {
-    const {userId} = auth();
-    const currentUrl = new URL(req.url);
-    const isAccessingDashboard = currentUrl.pathname === '/home';
-    const isApiRequest = currentUrl.pathname.startsWith('/api');
+  const { userId } = auth();
+  const currentUrl = new URL(req.url);
+  const isAccessingDashboard = currentUrl.pathname === '/home';
+  const isApiRequest = currentUrl.pathname.startsWith('/api');
+  /*
+  shortcuts
+  - R = Rule
+  */
 
-    if(userId && )
+  //-----R-1 Already logged logged in getting the user's ID-----//
+  if (userId && isPublicRoutes && !isAccessingDashboard) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  };
+
+  //-----Not logged in-----//
+  if (!userId) {
+    if (!isPublicApiRoutes(req) && !isPublicRoutes(req)) {
+      return NextResponse.redirect(new URL("signin", req.url));
+    };
+  };
+
+  if(isApiRequest && !isPublicApiRoutes(req)){
+    return NextResponse.redirect(new URL ("/signin", req.url));
+  };
 })
 
 export const config = {
